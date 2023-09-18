e48846725cc290ff19083766dcb407d2896def85
Lawrence Caulker <locaulker@gmail.com>
2023-09-18 00:07:54 -0400

    refresh tours. end application

--- src/App.jsx
+++ src/App.jsx
@@ -38,6 +38,24 @@ const App = () => {
   }
   // TODO
 
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>No tours left</h2>
+          <button
+            type="button"
+            style={{ marginTop: "2rem" }}
+            className="btn"
+            onClick={fetchedTours}
+          >
+            Refresh Tours
+          </button>
+        </div>
+      </main>
+    )
+  }
+
   return (
     <main>
       <Tours tours={tours} removeTour={removeTour} />
